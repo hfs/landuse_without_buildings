45850fef07f13e33f8456bd90c2d0fa9c4112826
Hermann Schwarting <hfs@gmx.de>
2021-02-04 01:04:45 +0100

    Recreate database each time

--- 02_createdb.sh
+++ 02_createdb.sh
@@ -4,5 +4,6 @@ source env.sh
 # First PostgreSQL startup takes a bit longer
 ./wait_for_postgres.sh
 echo ">>> Create database '$PGDATABASE'"
+dropdb --if-exists $PGDATABASE
 createdb $PGDATABASE
 psql -c "CREATE EXTENSION postgis"
