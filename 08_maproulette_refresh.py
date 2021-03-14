3ce6cf9c17716910aa187b75c827d3c596dd9dad
Hermann Schwarting <hfs@gmx.de>
2021-03-14 22:06:40 +0100

    MapRoulette refresh: Continue on 504
    Sometimes -- presumably when the refresh takes too long -- the
    MapRoulette API would respond with an HTTP error 504 "Gateway Timeout".
    In the web frontend one can see, though, that the rebuilding of the
    challenge is under way. So, assuming that the refresh is triggered as
    background process, continue anyway when receiving a 504 error.

--- 08_maproulette_refresh.py
+++ 08_maproulette_refresh.py
@@ -28,9 +28,25 @@ project_challenges = project_api.get_project_challenges(PROJECT_ID, limit=100)['
 for challenge in project_challenges:
     if challenge['enabled'] and challenge['status'] == 3:
         logging.info("Rebuilding challenge %d", challenge['id'])
-        result = challenge_api.rebuild_challenge(challenge['id'], remove_unmatched=True)
+        result = None
+        try:
+            result = challenge_api.rebuild_challenge(challenge['id'], remove_unmatched=True)
+        except maproulette.api.errors.HttpError as err:
+            # Apparently this can happen? Rebuilding the tasks will then
+            # continue in the background anyway, so we can ignore it and
+            # continue.
+            if err.status == 504:
+                logging.warning("HTTP Error %d when rebuilding challenge %d: %s", err.status, challenge['id'], err.message)
+            else:
+                raise err
         logging.info("Rebuilt challenge: %s", result)
         time.sleep(30)
-        result = challenge_api.update_challenge(challenge['id'], {'dataOriginDate': osmdump_mtime.isoformat()})
+        try:
+            result = challenge_api.update_challenge(challenge['id'], {'dataOriginDate': osmdump_mtime.isoformat()})
+        except maproulette.api.errors.HttpError as err:
+            if err.status == 504:
+                logging.warning("HTTP Error %d when updating challenge %d: %s", err.status, challenge['id'], err.message)
+            else:
+                raise err
         logging.info("Updated challenge: %s", result)
         time.sleep(30) # Give it a bit time to rebuild the tasks
