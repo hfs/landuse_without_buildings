c499c4d9d8a6b06629abe5cb1b66226b801eec3d
Hermann Schwarting <hfs@gmx.de>
2021-03-20 16:58:27 +0100

    Upload: force move files
    Using -f might be necessary when using Docker, because generated files
    will be owned by root.

--- 07_upload_results.sh
+++ 07_upload_results.sh
@@ -2,14 +2,14 @@
 
 DATE=$(date -d @$(stat --format %Y data/germany-latest.osm.pbf) +%Y-%m-%d)
 
-mv data/*.csv ../landuse_without_buildings_csv/
+mv -f data/*.csv ../landuse_without_buildings_csv/
 pushd ../landuse_without_buildings_csv/
 git add -u
 git commit -m "Update with data from $DATE"
 git push
 popd
 
-mv data/*.geojson ../landuse_without_buildings_geojson/
+mv -f data/*.geojson ../landuse_without_buildings_geojson/
 pushd ../landuse_without_buildings_geojson/
 git add -u
 git commit -m "Update with data from $DATE"
