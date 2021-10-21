f69ecf60440d3f7f3c0fab59efee58946a6d9a3d
Hermann Schwarting <hfs@gmx.de>
2021-10-21 23:46:12 +0200

    Use pg_isready
    instead of a self-made solution

--- wait_for_postgres.sh
+++ wait_for_postgres.sh
@@ -4,8 +4,8 @@ source env.sh
 
 echo -n ">>> Waiting for PostgreSQL to become available"
 for try in {0..180}; do
-    if psql -d postgres -c "SELECT 1" &> /dev/null; then
-        echo " ok"
+    if pg_isready -q -d postgres; then
+        echo " -> ok"
         exit 0
     fi
     echo -n .
