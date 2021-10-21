54a635778f3e072dfb81b49b58fc108f25c1977d
Hermann Schwarting <hfs@gmx.de>
2021-10-21 23:52:28 +0200

    Single file CSV and GeoJSON export
    Simplify the file exports again by exporting the tasks into only one CSV
    or GeoJSON file, respectively.

--- 05_export_csv.sh
+++ 05_export_csv.sh
@@ -1,8 +1,4 @@
 #!/bin/bash -e
 source env.sh
-echo ">>> Export results as one CSV table per state"
-for state in Baden-Württemberg Bayern Berlin Brandenburg Bremen Hamburg Hessen Mecklenburg-Vorpommern Niedersachsen Nordrhein-Westfalen Rheinland-Pfalz Saarland Sachsen Sachsen-Anhalt Schleswig-Holstein Thüringen
-do
-    echo "Landkreis/kreisfreie Stadt/Stadtbezirk,URL,Fläche" > data/$state.csv
-    psql -t -c "\COPY (SELECT county, 'https://www.openstreetmap.org/' || (CASE WHEN area_id < 0 THEN 'relation/' || (-area_id)::text ELSE 'way/' || area_id::text END) || '/' AS url, ROUND(area)::int AS area FROM landuse_export WHERE state = '$state' ORDER BY county, area DESC, url) TO STDOUT (FORMAT csv)" >> data/$state.csv
-done
+echo ">>> Export results as CSV file"
+psql -f export_csv.sql -t > data/landuse_without_buildings.csv
