0172e4a5bba2afdea79e46b30e88926f3e4fbaa7
Hermann Schwarting <hfs@gmx.de>
2021-10-29 09:09:59 +0200

    Evaluate more building types
    Also evaluate some amenity tags like hospital or nursing_home and some
    farm stuff like bunker_silo and storage_tank to reduce the number of
    false positives.

--- residential_and_buildings.lua
+++ residential_and_buildings.lua
@@ -23,14 +23,35 @@ tables.highway = osm2pgsql.define_way_table('highway', {
     { column = 'geom', type = 'linestring', projection=3035 },
 })
 
+function building_type(object)
+    building_value = object.tags.building or object.tags['building:part'] or
+        object.tags['abandoned:building'] or object.tags['demolished:building'] or
+        object.tags['removed:building'] or object.tags['razed:building']
+    if object.tags.amenity then
+        amenity = object.tags.amenity
+        if amenity == 'hospital' or amenity == 'nursing_home' or
+                amenity == 'prison' or amenity == 'school' or
+                amenity == 'social_facility' then
+            building_value = amenity
+        end
+    end
+    if object.tags.man_made then
+        man_made = object.tags.man_made
+        if man_made == 'bunker_silo' or man_made == 'storage_tank' or
+                man_made == 'wastewater_plant' then
+            building_value = man_made
+        end
+    end
+    return building_value
+end
+
+
 function osm2pgsql.process_way(object)
     if object.tags.landuse then
         row = { geom = { create = 'area' }, landuse = object.tags.landuse }
         tables.landuse:add_row(row)
     end
-    building_value = object.tags.building or object.tags['building:part'] or
-        object.tags['abandoned:building'] or object.tags['demolished:building'] or
-        object.tags['removed:building'] or object.tags['razed:building']
+    building_value = building_type(object)
     if building_value then
         row = { geom = { create = 'area' }, building = building_value }
         tables.building:add_row(row)
@@ -45,9 +66,7 @@ function osm2pgsql.process_relation(object)
         row = { geom = { create = 'area' }, landuse = object.tags.landuse }
         tables.landuse:add_row(row)
     end
-    building_value = object.tags.building or object.tags['building:part'] or
-        object.tags['abandoned:building'] or object.tags['demolished:building'] or
-        object.tags['removed:building'] or object.tags['razed:building']
+    building_value = building_type(object)
     if object.tags.type == 'multipolygon' and building_value then
         row = { geom = { create = 'area' }, building = building_value }
         tables.building:add_row(row)
