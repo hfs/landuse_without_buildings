770cefd9c60439583c7aebaece76c61867e503b9
Hermann Schwarting <hfs@gmx.de>
2022-06-19 22:56:04 +0200

    Export as single JSON file

---
+++ export_geojson.sql
@@ -0,0 +1,19 @@
+SELECT json_build_object(
+    'type', 'FeatureCollection',
+    'features', json_agg(ST_AsGeoJSON(u.*)::json)
+)
+FROM (
+    SELECT
+        id,
+        osm_id,
+        ROUND(area) AS area,
+        ROUND(building_fraction::numeric, 4) AS building_fraction,
+        -- No idea why ForceRHR doesn't help
+        ST_ForceRHR(ST_Transform(geom, 4326)) AS geom
+    FROM landuse_split
+    WHERE
+        building_fraction <= 0.05
+    ORDER BY id
+) u
+;
+
